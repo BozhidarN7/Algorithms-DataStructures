035185b7b464ee9a1a77d20e4a1d17c04ebadc01
BozhidarN7 <bojonemski@gmail.com>
2020-12-20 15:29:48 +0200

    Another two Strings problems solved from HackerRank

---
+++ HackerRank/Strings/HackerRankInAString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HackerRankInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int queries = int.Parse(Console.ReadLine());
+            string pattern = "hackerrank";
+            for (int q = 0; q < queries; q++)
+            {
+                bool contains = false;
+                string s = Console.ReadLine();
+                int patternIndex = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == pattern[patternIndex])
+                    {
+                        patternIndex++;
+                    }
+
+                    if (patternIndex == pattern.Length - 1)
+                    {
+                        Console.WriteLine("YES");
+                        contains = true;
+                        break;
+                    }
+                }
+
+                if (!contains)
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
