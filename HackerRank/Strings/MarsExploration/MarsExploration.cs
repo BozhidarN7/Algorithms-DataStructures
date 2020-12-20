46b93acb34504c68ce485bec955f2b6452215a19
BozhidarN7 <bojonemski@gmail.com>
2020-12-20 14:53:52 +0200

    Another String problem solved from HackerRank

---
+++ HackerRank/Strings/MarsExploration/MarsExploration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MarsExploration
+{
+    class MarsExploration
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int count = 1;
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (count == 1 && s[i] != 'S')
+                {
+                    result++;
+                }
+                else if (count == 2 && s[i] != 'O')
+                {
+                    result++;
+                }
+                else if (count == 3 && s[i] != 'S')
+                {
+                    result++;
+                }
+
+                if (count == 3)
+                {
+                    count = 1;
+                    continue;
+                }
+                count++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
