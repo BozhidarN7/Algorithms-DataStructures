2e6851d34a0c1304575c9999b9d140799f9ef768
BozhidarN7 <bojonemski@gmail.com>
2020-12-20 15:08:52 +0200

    Another String problem solved from HackerRank

---
+++ HackerRank/Strings/SuperReducedString/SuperReducedString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SuperReducedString
+{
+    class SuperReducedString
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Regex pattern = new Regex(@"([a-z])\1");
+            MatchCollection matches = pattern.Matches(s);
+
+            foreach (Match match in matches)
+            {
+                s = s.Replace(match.Groups[0].Value, string.Empty);
+            }
+
+            if (s == string.Empty)
+            {
+                Console.WriteLine("Empty String");
+            }
+            else
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
