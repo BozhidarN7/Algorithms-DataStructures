78815351cd12312176cbe456f4f04061ce511537
BozhidarN7 <bojonemski@gmail.com>
2020-12-22 19:09:23 +0200

    Another String problem from HackerRank (not solved)

---
+++ HackerRank/Strings/SeparateTheNumbers/SeparateTheNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SeparateTheNumbers
+{
+    class SeparateTheNumbers
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+            while (q != 0)
+            {
+                string s = Console.ReadLine();
+
+
+
+                int firstNumber = s[0] - '0';
+                int index = 1;
+                int length = 1;
+                int nextNumber = 0;
+
+                while (true)
+                {
+                    nextNumber = int.Parse(s.Substring(index, length));
+                    if (firstNumber == nextNumber - 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        firstNumber = firstNumber * 10 + (int)(s[index] - '0');
+                        length++;
+                        index++;
+                    }
+                }
+                Console.WriteLine(firstNumber);
+
+                q--;
+            }
+        }
+    }
+}
