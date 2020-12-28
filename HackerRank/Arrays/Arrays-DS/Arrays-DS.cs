f0aedb351cdacecde7bc3ee48658d7a86ce9e14c
BozhidarN7 <bojonemski@gmail.com>
2020-12-28 15:44:10 +0200

    The first Array problem solved from HackerRank

---
+++ HackerRank/Arrays/Arrays-DS/Arrays-DS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(" ", arr.Reverse()));
+        }
+    }
+}
