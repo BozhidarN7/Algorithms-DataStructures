4f4cb0719e382517aeef0254f290b48af8139a3a
BozhidarN7 <bojonemski@gmail.com>
2020-12-28 17:23:14 +0200

    Another Array problem solved from HackerRank

---
+++ HackerRank/Arrays/LeftRotation/LeftRotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace LeftRotation
+{
+    class LeftRotation
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int d = tokens[1];
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] newArr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = i - d % arr.Length >= 0 ? i - d : arr.Length - (d - i) % arr.Length ;
+                newArr[index] = arr[i];
+            }
+
+            Console.WriteLine(string.Join(" ", newArr));
+
+        }
+    }
+}
