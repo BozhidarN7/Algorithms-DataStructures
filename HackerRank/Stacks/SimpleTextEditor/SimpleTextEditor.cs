896fd98b03133e6e2a381427b88982cbd54f2881
BozhidarN7 <bojonemski@gmail.com>
2020-12-31 16:33:11 +0200

    Another Stack problem solved from HackerRank

---
+++ HackerRank/Stacks/SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleTextEditor
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+            Stack<string> lastState = new Stack<string>();
+            while (q > 0)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                int command = int.Parse(tokens[0]);
+                if (command == 1)
+                {
+                    lastState.Push(text);
+                    text += tokens[1];
+                }
+                else if (command == 2)
+                {
+                    int k = int.Parse(tokens[1]);
+                    lastState.Push(text);
+                    text = text.Remove(text.Length - k);
+                }
+                else if (command == 3)
+                {
+                    int k = int.Parse(tokens[1]);
+                    Console.WriteLine(text[k - 1]);
+                }
+                else
+                {
+                    if (lastState.Count != 0)
+                    {
+                        text = lastState.Pop();
+                    }
+
+                }
+                q--;
+            }
+        }
+    }
+}
