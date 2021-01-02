9eaa1264340c49a5492e3053eb1ec8ec4d9130ff
BozhidarN7 <bojonemski@gmail.com>
2021-01-02 09:44:08 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/BillDivision/BillDivision.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace BillDivision
+{
+    class BillDivision
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int k = tokens[1];
+
+            int[] bills = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int chargedValue = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+            for (int i = 0;i < bills.Length; i++)
+            {
+                if (k == i)
+                {
+                    continue;
+                }
+
+                totalSum += bills[i];
+            }
+
+            if (totalSum / 2== chargedValue)
+            {
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                Console.WriteLine(chargedValue  - totalSum /2);
+            }
+        }
+    }
+}
