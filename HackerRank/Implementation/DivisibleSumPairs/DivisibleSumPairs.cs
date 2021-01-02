0ae59b9b659f84578c38f36517f7e79034e1c2ad
BozhidarN7 <bojonemski@gmail.com>
2021-01-02 08:33:27 +0200

    Solved the first Implementation problem from HackerRank

---
+++ HackerRank/Implementation/DivisibleSumPairs/DivisibleSumPairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DivisibleSumPairs
+{
+    class DivisibleSumPairs
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int k = tokens[1];
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int result = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if ((arr[i] + arr[j]) % k == 0)
+                    {
+                        result++;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
