32f21efae4672b5f2a49e18a94526d6c16cb651c
BozhidarN7 <bojonemski@gmail.com>
2021-01-03 08:19:27 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/SalesByMatch/SalesByMatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    class SalesByMatch
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] pairs = new int[101];
+
+            for (int i = 0; i < n; i++)
+            {
+                pairs[arr[i]]++;
+            }
+            int result = 0;
+            for (int i = 0; i < 101; i++)
+            {
+                if (pairs[i] != 0)
+                {
+                    result += pairs[i] / 2;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
