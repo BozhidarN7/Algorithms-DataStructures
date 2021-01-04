da17039455f1bf327439db612ed5ddb218733542
BozhidarN7 <bojonemski@gmail.com>
2021-01-04 10:23:50 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/CatsAndAMouse/CatsAndAMouse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CatsAndAMouse
+{
+    class CatsAndAMouse
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+            while (q > 0)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int x = tokens[0];
+                int y = tokens[1];
+                int z = tokens[2];
+
+                int catASteps = Math.Abs(x - z);
+                int catBSteps = Math.Abs(y - z);
+
+                if (catASteps < catBSteps)
+                {
+                    Console.WriteLine("Cat A");
+                }
+                else if (catBSteps < catASteps)
+                {
+                    Console.WriteLine("Cat B");
+                }
+                else
+                {
+                    Console.WriteLine("Mouse C");
+                }
+
+                q--;
+            }
+        }
+    }
+}
