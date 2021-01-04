bf15b4c45af179a365481850e8f2851d9c5fc19b
BozhidarN7 <bojonemski@gmail.com>
2021-01-04 09:02:43 +0200

    Another Implementation problem solved from HackerRank

--- HackerRank/Implementation/DrawingBook/DrawingBook.cs
+++ HackerRank/Implementation/DrawingBook/DrawingBook.cs
@@ -9,55 +9,7 @@ namespace DrawingBook
             int n = int.Parse(Console.ReadLine());
             int p = int.Parse(Console.ReadLine());
 
-            if (p == 1)
-            {
-                Console.WriteLine(0);
-                return;
-            }
-
-            int leftPage = 2;
-            int rightPage = 3;
-            int turnsFromTheBeginning = 1;
-            while (true)
-            {
-                if (leftPage == p || rightPage == p)
-                {
-                    break;
-                }
-                leftPage += 2;
-                rightPage += 2;
-                turnsFromTheBeginning++;
-            }
-
-            if (p == n || (n % 2 != 0 && n - 1 == p))
-            {
-                Console.WriteLine(0);
-                return;
-            }
-
-            if (n % 2 == 0)
-            {
-                leftPage = n - 2;
-                rightPage = n - 1;
-            }
-            else
-            {
-                leftPage = n - 2;
-                rightPage = n - 2;
-            }
-            int turnsFromTheEnd = 0;
-            while (true)
-            {
-                if (leftPage == p || rightPage == p)
-                {
-                    break;
-                }
-                leftPage -= 2;
-                rightPage -= 2;
-                turnsFromTheEnd++;
-            }
-
-            Console.WriteLine(Math.Max(turnsFromTheBeginning, turnsFromTheEnd));
+            Console.WriteLine(Math.Min(p / 2, n / 2 - p / 2));
         }
     }
 }
---
+++ HackerRank/Implementation/ElectronicsShop/ElectronicsShop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ElectronicsShop
+{
+    class ElectronicsShop
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int b = tokens[0];
+            int n = tokens[1];
+            int m = tokens[2];
+            int[] keyboards = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] drives = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int mostExpensive = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (keyboards[i] + drives[j] <= b)
+                    {
+                        mostExpensive = Math.Max(mostExpensive, keyboards[i] + drives[j]);
+                    }
+                }
+            }
+            if (mostExpensive != 0)
+            {
+                Console.WriteLine(mostExpensive);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
