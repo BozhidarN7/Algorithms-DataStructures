01fc1b91f588751a6fd65c3f584c8b32201b2174
BozhidarN7 <bojonemski@gmail.com>
2021-01-05 09:40:55 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/PickingNumbers/PickingNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace PickingNumbers
+{
+    class PickingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] occ = new int[100];
+            for (int i = 0; i < n; i++)
+            {
+                occ[arr[i]]++;
+            }
+            int result = 0;
+            for (int i = 0; i < n -1; i++)
+            {
+                if (occ[i] + occ[i +1]> result)
+                {
+                    result = occ[i] + occ[i + 1];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
