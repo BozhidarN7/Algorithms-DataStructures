0ef6b5f5675551e9dc692283b38c0daf331c2e09
BozhidarN7 <bojonemski@gmail.com>
2021-01-06 07:54:14 +0200

    Another Implementation problem solved from HackeRank

---
+++ HackerRank/Implementation/TheHurdleRace/TheHurdleRace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace TheHurdleRace
+{
+    class TheHurdleRace
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int k = tokens[1];
+
+            int[] heights = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(heights.Max() - k);
+        }
+    }
+}
