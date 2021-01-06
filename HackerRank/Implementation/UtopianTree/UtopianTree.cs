f3e44ab5fa933cbca78655f0c09d26636a47f6c3
BozhidarN7 <bojonemski@gmail.com>
2021-01-06 08:34:37 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/UtopianTree/UtopianTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class UtopianTree
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                int n = int.Parse(Console.ReadLine());
+                int height = 1;
+                int season = 0; // 0 - spring, 1 - summer
+                for (int i = 0; i< n; i++)
+                {
+                    if (season == 0)
+                    {
+                        height *= 2;
+                        season = 1;
+                    }
+                    else if( season == 1)
+                    {
+                        height += 1;
+                        season = 0;
+                    }
+                }
+                Console.WriteLine(height);
+                t--;
+            }
+        }
+    }
+}
