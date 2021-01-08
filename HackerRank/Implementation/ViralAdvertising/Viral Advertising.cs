65945a02e3b75a64d3b34720624c8e4cca1601c2
BozhidarN7 <bojonemski@gmail.com>
2021-01-08 08:47:12 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/ViralAdvertising/Viral Advertising.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalLikes = 0;
+            int people = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                int newLikes = (int)Math.Floor((decimal)people / 2);
+                totalLikes += newLikes;
+                people = newLikes * 3;
+            }
+            Console.WriteLine(totalLikes);
+        }
+    }
+}
