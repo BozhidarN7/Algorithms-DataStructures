b2ee7ebe2c8d4cc6f0e02972bb809d6e384be025
BozhidarN7 <bojonemski@gmail.com>
2021-01-09 16:41:26 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/JumpingOnTheCloudsRevisited/JumpingOnTheCloudsRevisited.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace JumpingOnTheCloudsRevisited
+{
+    class JumpingOnTheCloudsRevisited
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int k = tokens[1];
+            int energy = 100;
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int index = k % n;
+            while (index != 0)
+            {
+                energy = CalculateEnergyLost(arr, index, energy);
+                index = (index + k) % n;
+            }
+            energy = CalculateEnergyLost(arr, index, energy);
+
+            Console.WriteLine(energy);
+        }
+        private static int CalculateEnergyLost(int[] arr, int index, int energy)
+        {
+            energy -= 1;
+            if (arr[index] == 1)
+            {
+                energy -= 2;
+            }
+            return energy;
+        }
+    }
+}
