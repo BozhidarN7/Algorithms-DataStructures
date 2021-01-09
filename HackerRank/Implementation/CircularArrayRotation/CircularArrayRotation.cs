029ba773e8f4a38571bf5df5db0c46c1b220680f
BozhidarN7 <bojonemski@gmail.com>
2021-01-09 16:13:14 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/CircularArrayRotation/CircularArrayRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CircularArrayRotation
+{
+    class CircularArrayRotation
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = tokens[0];
+            int k = tokens[1];
+            int q = tokens[2];
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] rotatedArr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                rotatedArr[(i + k) % n] = arr[i];
+            }
+
+            while (q > 0)
+            {
+                int query = int.Parse(Console.ReadLine());
+                Console.WriteLine(rotatedArr[query]);
+                q--;
+
+            }
+        }
+    }
+}
