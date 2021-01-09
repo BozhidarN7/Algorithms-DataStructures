63b8f7536bdc1e7b445986e7cdb96e2625533733
BozhidarN7 <bojonemski@gmail.com>
2021-01-09 08:31:08 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/SaveThePrisoner/Save the Prisoner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace SaveThePrisoner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int n = tokens[0];
+                int m = tokens[1];
+                int s = tokens[2];
+
+                int chairNumber = (s - 1 + m - 1 )%n;
+                Console.WriteLine(chairNumber + 1);
+
+                t--;
+            }
+        }
+    }
+}
