68d007c957f29b4d0f90b12bf793608c2227b1f1
BozhidarN7 <bojonemski@gmail.com>
2021-01-09 16:26:14 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/SequenceEquation/SequenceEquation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace SequenceEquation
+{
+    class SequenceEquation
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = i + 1;
+                int neededNumber = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[j] == currentNumber)
+                    {
+                        neededNumber = j + 1;
+                        break;
+                    }
+                }
+                int y = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[j] == neededNumber)
+                    {
+                        y = j + 1;
+                        break;
+                    }
+                }
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
