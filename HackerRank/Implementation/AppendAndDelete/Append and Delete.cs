3ee8d7873dab0725e1f847d0771b7daa47d53f6a
BozhidarN7 <bojonemski@gmail.com>
2021-01-10 08:24:19 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/AppendAndDelete/Append and Delete.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AppendAndDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = Console.ReadLine();
+            int equalCharacters = 0;
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < t.Length && s[i] == t[i])
+                {
+                    equalCharacters++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (s.Length + t.Length - 2 * equalCharacters > k)
+            {
+                Console.WriteLine("No");
+            }
+            else if ((s.Length + t.Length - 2 * equalCharacters) % 2 == k % 2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if ((s.Length + t.Length - k) < 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
