f617104d64a8d4dfff7dbac3f4161fd2c326997e
BozhidarN7 <bojonemski@gmail.com>
2021-01-11 08:47:45 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/CutThesticks/CutThesticks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CutThesticks
+{
+    class CutThesticks
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sticks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int remained = n;
+            Console.WriteLine(remained);
+            while (true)
+            {
+                int minLength = sticks.Where(x => x > 0).Min();
+                for (int i = 0; i < n; i++)
+                {
+                    sticks[i] -= minLength;
+                }
+                remained = sticks.Where(x => x > 0).Count();
+
+                if (remained == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine(remained);
+            }
+        }
+    }
+}
