d468ca74a2226139e97b73c6672211453870df69
BozhidarN7 <bojonemski@gmail.com>
2021-01-11 09:10:54 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/RepeatedString/RepeatedString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace RepeatedString
+{
+    class RepeatedString
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            long n = long.Parse(Console.ReadLine());
+
+            long a = s.Where(x => x == 'a').Count();
+            long repetitions = n % s.Length == 0 ? n / s.Length : n / s.Length + 1;
+            long skip = (repetitions * s.Length) % n;
+            long total = repetitions * a;
+            for (int i = Convert.ToInt32(s.Length - skip); i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    total--;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
