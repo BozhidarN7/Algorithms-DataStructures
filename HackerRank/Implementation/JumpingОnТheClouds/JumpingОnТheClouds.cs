f52862da620c9bf295d3643678e6f27abcfe7f28
BozhidarN7 <bojonemski@gmail.com>
2021-01-12 08:59:18 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/JumpingОnТheClouds/JumpingОnТheClouds.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace JumpingОnТheClouds
+{
+    class JumpingОnТheClouds
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] clouds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int jumps = 0;
+            int index = 0;
+            while (index < n - 2)
+            {
+                if (clouds[index + 2] == 0)
+                {
+                    index += 2;
+                }
+                else if (clouds[index + 1] == 0)
+                {
+                    index += 1;
+                }
+                jumps++;
+            }
+            if (index == n - 2)
+            {
+                jumps++;
+            }
+            Console.WriteLine(jumps);
+        }
+    }
+}
