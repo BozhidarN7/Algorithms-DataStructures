0982ab54a945167efa5a950512585c0f0ec04547
BozhidarN7 <bojonemski@gmail.com>
2021-01-13 08:50:19 +0200

    Queue problem solved from HackerRank

---
+++ HackerRank/Queues/QueueUsingTwoStacks/QueueUsingTwoStacks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueueUsingTwoStacks
+{
+    class QueueUsingTwoStacks
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+
+            Stack<int> rear = new Stack<int>();
+            Stack<int> front = new Stack<int>();
+            while (q > 0)
+            {
+                int[] tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int operation = tokens[0];
+                if (operation == 1)
+                {
+                    rear.Push(tokens[1]);
+                }
+                else
+                {
+                    if (front.Count == 0)
+                    {
+                        while (rear.Count != 0)
+                        {
+                            front.Push(rear.Pop());
+                        }
+                    }
+                    if (front.Count != 0)
+                    {
+                        if (operation == 2)
+                        {
+                            front.Pop();
+                        }
+                        else
+                        {
+                            Console.WriteLine(front.Peek());
+                        }
+                    }
+                }
+                q--;
+            }
+        }
+    }
+}
