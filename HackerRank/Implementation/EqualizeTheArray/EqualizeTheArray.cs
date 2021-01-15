568e8a7a98a981ec051987f5576565c95350ee95
BozhidarN7 <bojonemski@gmail.com>
2021-01-15 12:02:37 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/EqualizeTheArray/EqualizeTheArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualizeTheArray
+{
+    class EqualizeTheArray
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] occ = new int[101];
+            for (int i = 0; i < n; i++)
+            {
+                occ[arr[i]]++;
+            }
+            int mostCommonValue = occ.Max();
+            int mostCommonElement = arr.Where(x => occ[x] == mostCommonValue).ToArray()[0];
+            int removed = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i != mostCommonElement)
+                {
+                    removed += occ[i];
+                }
+            }
+            Console.WriteLine(removed);
+        }
+    }
+}
