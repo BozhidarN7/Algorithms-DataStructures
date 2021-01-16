36c017442d064fdc521c50e9f8b19577dc5150b7
BozhidarN7 <bojonemski@gmail.com>
2021-01-16 15:12:32 +0200

    Another Implementation problem solved from HackerRank

---
+++ HackerRank/Implementation/MinimumDistances/MinimumDistances.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumDistances
+{
+    class MinimumDistances
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int difference = int.MaxValue;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        difference = Math.Min(difference, j - i);
+                    }
+                }
+            }
+            if (difference == int.MaxValue)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(difference);
+            }
+
+        }
+    }
+}
