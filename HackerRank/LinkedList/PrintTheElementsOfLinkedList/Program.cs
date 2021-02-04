99de410e477fe753af5d9c7c17181a6878f0c74d
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-04 11:18:10 +0200

    Added Linked list problem

---
+++ HackerRank/LinkedList/PrintTheElementsOfLinkedList/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace PrintTheElementsOfLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            SinglyLinkedList.printLinkedList(llist.head);
+        }
+    }
+    public class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    public class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+        public static void printLinkedList(SinglyLinkedListNode head)
+        {
+            SinglyLinkedListNode currentNode = head;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+            }
+
+        }
+    }
+}
