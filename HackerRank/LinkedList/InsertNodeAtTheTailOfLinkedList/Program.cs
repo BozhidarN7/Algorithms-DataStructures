4c7f40b0b127444fe80c6969c168d6214c09848c
Bozhidar Nemski <bojonemski@gmail.com>
2021-02-04 11:30:31 +0200

    Added Linked list problem

---
+++ HackerRank/LinkedList/InsertNodeAtTheTailOfLinkedList/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System;
+
+class Solution
+{
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+        }
+
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+    {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+
+        if (head == null)
+        {
+            head = newNode;
+            return head;
+        }
+        SinglyLinkedListNode lastNode = head;
+        while (lastNode.next != null)
+        {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < llistCount; i++)
+        {
+            int llistItem = Convert.ToInt32(Console.ReadLine());
+            SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+            llist.head = llist_head;
+
+        }
+
+
+
+        PrintSinglyLinkedList(llist.head, "\n", textWriter);
+        textWriter.WriteLine();
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
