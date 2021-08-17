31cc4dcd62a716aa8a64df99414fe63e3c364ca2
Bozhidar Nemski <bojonemski@gmail.com>
2021-08-17 21:38:41 +0300

    Added tho problems

---
+++ HackerRank/InterviewPreparationKit/HashTableRansomNote/HashTableRansomNote.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashTableRansomNote
+{
+    class HashTableRansomNote
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int m = input[0];
+            int n = input[1];
+
+            Dictionary<string, int> magazineWords = new Dictionary<string, int>();
+            ReadInput(magazineWords, m);
+
+            Dictionary<string, int> noteWords = new Dictionary<string, int>();
+            ReadInput(noteWords, n);
+
+            Console.WriteLine(CheckMagazine(noteWords, magazineWords));
+        }
+
+        private static string CheckMagazine(Dictionary<string, int> noteWords, Dictionary<string, int> magazineWords)
+        {
+            foreach (KeyValuePair<string,int> kvp in noteWords)
+            {
+                if (!magazineWords.ContainsKey(kvp.Key))
+                {
+                    return "No";
+                }
+
+                if (magazineWords[kvp.Key] < kvp.Value)
+                {
+                    return "No";
+                }
+            }
+
+            return "Yes ";
+        }
+        private static void ReadInput(Dictionary<string, int> dic, int n)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = words[i];
+
+                if (!dic.ContainsKey(word))
+                {
+                    dic.Add(word, 0);
+                }
+
+                dic[word]++;
+            }
+        }
+    }
+}
---
+++ HackerRank/InterviewPreparationKit/TwoStrings/TwoStrings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoStrings
+{
+    class TwoStrings
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            for (int t = 0; t < p; t++)
+            {
+                string str1 = Console.ReadLine();
+                string str2 = Console.ReadLine();
+
+                HashSet<char> str1Characters = new HashSet<char>();
+                GetCharacters(str1, str1Characters);
+
+                HashSet<char> str2Characters = new HashSet<char>();
+                GetCharacters(str2, str2Characters);
+
+                Console.WriteLine(CheckForAtLeatOneCommonSymbol(str1Characters, str2Characters));
+
+            }
+        }
+
+        private static string CheckForAtLeatOneCommonSymbol(HashSet<char> str1Characters, HashSet<char> str2Characters)
+        {
+            foreach (char symbol in str1Characters)
+            {
+                if (str2Characters.Contains(symbol))
+                {
+                    return "YES";
+                }
+            }
+
+            return "NO";
+        }
+
+        private static void GetCharacters(string str, HashSet<char> set)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                set.Add(str[i]);
+            }
+        }
+    }
+}
